88470f685ae2062545ba0bdcb21e6fe50f53e258
itsJoel59 <joel.j.jansson@gmail.com>
2025-06-01 15:16:24 +0200

    Installed Pug and set it up for static file serving

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './templates');
 app.get('/', (request, response) => {
-    response.send('Server is running!');
+    response.render('index');
 });
+
+app.use('/static', express.static('./static'));
 
 app.listen(5080);
