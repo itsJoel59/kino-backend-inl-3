3f7da8930495b7480850ed3160735d4f79552c97
itsJoel59 <joel.j.jansson@gmail.com>
2025-06-03 17:07:49 +0200

    Created pug templates as well as routes for movies and movie

--- server.js
+++ server.js
@@ -4,8 +4,37 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './templates');
+
 app.get('/', (request, response) => {
-    response.render('index');
+    response.render('home');
+});
+
+app.get('/movies', async (request, response) => {
+    try {
+        const res = await fetch('https://plankton-app-xhkom.ondigitalocean.app/api/movies');
+        const movies = await res.json();
+        response.render('movies', { movies });
+    } catch (err) {
+        console.error(err);
+        response.status(500).send('Error fetching movie list');
+    }
+});
+
+app.get('/movie/:id', async (request, response) => {
+    const { id } = request.params; // Same as ' const id = request.params.id; '
+    try {
+        const res = await fetch(`https://plankton-app-xhmkom.ondigitalocean.app/api/movies/${id}`);
+        if (!response.ok) {
+            throw new Error('Movie not found');
+        }
+
+        const movie = await response.json();
+        response.render('movie', { movie });
+
+    } catch (err) {
+        console.error(err);
+        response.status(404).render('error', { message: 'Movie not found', statusCode: 404 });
+    }
 });
 
 app.use('/static', express.static('./static'));
