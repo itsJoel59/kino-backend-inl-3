75b5d26d4214ea0f38f87a43ea80f2914011b5c7
itsJoel59 <joel.j.jansson@gmail.com>
2025-06-13 11:59:18 +0200

    Added function that posts a review to cms

--- cms.js
+++ cms.js
@@ -15,3 +15,30 @@ export async function fetchReviews(movieId, page = 1, pageSize = 5) {
     const data = await response.json();
     return data;
 }
+
+export async function postReview(movieId, name, rating, comment = '') {
+    
+    const response = await fetch(`${CMS_BASE}/reviews`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            data: {
+                movie: movieId,
+                name,
+                rating,
+                comment
+            }
+        })
+    });
+
+    if (!response.ok) {
+        const errData = await response.json().catch(() => ({}));    //Assigns an empty object to the errData variable if response is empty or json isn't valid
+
+        throw new Error(
+            `CMS postReview failed: 
+            ${response.status} 
+            ${response.statusText} 
+            ${JSON.stringify(errData)}`
+        );
+    }
+}
